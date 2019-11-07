d1efdbbcde9b2136f7a65524529d36fd4e4feb30
Nelis <nelis.boucke@bnpparisbasfortis.com>
2019-11-07 14:25:10 +0100

    Switching to junit5 to allow parameterised tests

--- src/test/kotlin/be/codekata/TestsShouldGoHere.kt
+++ src/test/kotlin/be/codekata/TestsShouldGoHere.kt
@@ -1,13 +1,27 @@
 package be.codekata
 
+
 import org.hamcrest.CoreMatchers.equalTo
-import org.junit.Assert.assertThat
-import org.junit.Test
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
 
 class TestsShouldGoHere {
 
     @Test
     fun failingTest() {
         assertThat<Boolean>(false, equalTo<Boolean>(true))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "0, ZERO",
+        "1, ONE"
+    )
+    fun shouldReturnWord_When_Number(number: Int, word: String) {
+        assertEquals(word, "")
     }
 }
