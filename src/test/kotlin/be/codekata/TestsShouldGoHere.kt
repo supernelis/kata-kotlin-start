97370d1cb8bb4e313496c5ddbf6ca72a2dae4d5e
Nelis Boucké <nelis@archiwise.com>
2020-02-24 22:26:38 +0100

    Add mockk as mocking framework and an example test

--- src/test/kotlin/be/codekata/TestsShouldGoHere.kt
+++ src/test/kotlin/be/codekata/TestsShouldGoHere.kt
@@ -1,6 +1,8 @@
 package be.codekata
 
 
+import io.mockk.mockk
+import io.mockk.verify
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -23,5 +25,29 @@ class TestsShouldGoHere {
     )
     fun shouldReturnWord_When_Number(number: Int, word: String) {
         assertEquals(word, "")
+    }
+
+    @Test
+    internal fun mockThisThingWithMockk() {
+        val something = mockk<Something>(relaxed = true)
+
+        val someOtherThing = SomeOtherThing(something)
+        someOtherThing.triggerTest()
+
+        verify {
+            something.test()
+        }
+    }
+
+    class SomeOtherThing(private val something: Something) {
+        fun triggerTest() {
+            something.test()
+        }
+
+    }
+
+    class Something {
+        fun test() {
+        }
     }
 }
